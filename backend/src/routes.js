1614603ee73caf94f3400b3a3768932e68b90600
FlaviaBertoni <flavia.boava.bertoni@gmail.com>
2020-03-25 13:47:55 -0300

    Create express routes

---
+++ backend/src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const crypto = require('crypto');
+
+const routes = express.Router();
+
+routes.post('/ongs', (request, response) => {
+    const { name, email, whatsapp, city } = request.body;
+    
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    return response.json();
+});
+
+module.exports = routes;
