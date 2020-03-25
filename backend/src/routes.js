cb694f7882620911a5c3741ea483056289c06132
FlaviaBertoni <flavia.boava.bertoni@gmail.com>
2020-03-25 18:20:39 -0300

    add profile get route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
@@ -10,5 +11,7 @@ routes.post('/ongs', OngController.create);
 routes.get('/incidents',  IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile',  ProfileController.index);
 
 module.exports = routes;
