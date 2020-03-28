dd9093f45309836f966dc0e929544592086bf211
FlaviaBertoni <flavia.boava.bertoni@gmail.com>
2020-03-28 20:21:47 -0300

    Add logout feature

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -9,6 +9,8 @@ import './style.css';
 import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
+    const history = useHistory();
+
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
 
@@ -37,6 +39,11 @@ export default function Profile() {
         }    
     }
 
+    function handleLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+
     return ( 
         <div className="profile-container">
             <header>
@@ -44,7 +51,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar Novo Caso</Link>
-                <button type="button">
+                <button type="button" onClick={handleLogout}>
                     <FiPower size={18} color="#E02041" /> 
                 </button>
             </header>
