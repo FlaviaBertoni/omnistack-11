c10f649735778197d53879e4b51a867fe2868afe
FlaviaBertoni <flavia.boava.bertoni@gmail.com>
2020-03-28 20:10:12 -0300

    Add incident value with Intl ECMAScript native api

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -46,7 +46,9 @@ export default function Profile() {
                     <p>{incident.description}</p>
 
                     <strong>VALOR:</strong>
-                    <p>{incident.value}</p>
+                    <p>
+                        {Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}
+                    </p>
 
                     <button type="button">
                         <FiTrash2 size={20} color="a8a8b3"/>
