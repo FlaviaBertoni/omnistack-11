35a398495d187a49925a090e21f0555d80b29533
FlaviaBertoni <flavia.boava.bertoni@gmail.com>
2020-03-29 19:10:20 -0300

    Add back and mobile integration

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { Text, View, Image, TouchableOpacity, Linking } from 'react-native';
 import * as MailComposer from 'expo-mail-composer';
 
@@ -11,8 +11,17 @@ import logoImg from '../../assets/logo.png';
 export default function Detail() {
 
     const navigation = useNavigation();
+    const route = useRoute();
     
-    const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Nome do caso" com o valor de 120';
+    const { incident } = route.params;
+    const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso ${incident.title} com o valor de ${formatValue(incident.value)}`;
+
+    function formatValue(value) {
+        return Intl.NumberFormat('pt-BR', { 
+            style: 'currency', 
+            currency: 'BRL',
+        }).format(value)
+    }
 
     function navigateBack() {
         navigation.goBack();
@@ -20,15 +29,15 @@ export default function Detail() {
 
     function sendMail() {
         MailComposer.composeAsync({
-            subject: 'Herói do caso: Nome',
-            recipients: ['flavia-bertoni@hotmail.com'],
+            subject: `Herói do caso: ${incident.title}`,
+            recipients: [incident.email],
             body: message,
         });
     }
 
     function sendWhatsApp() {
         //this is a deeplink to open whatsapp
-        Linking.openURL(`whatsapp://send?text=${message}&phone=+5511955850220`);
+        Linking.openURL(`whatsapp://send?text=${message}&phone=${incident.whatsapp}`);
     }
 
     return (
@@ -42,13 +51,13 @@ export default function Detail() {
 
             <View style={styles.incident}>
                 <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-                <Text style={styles.incidentValue}>APAD</Text>
+                <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
                 <Text style={styles.incidentProperty}>CASO:</Text>
-                <Text style={styles.incidentValue}>Caso</Text>
+                <Text style={styles.incidentValue}>{incident.title}</Text>
 
                 <Text style={styles.incidentProperty}>VALOR:</Text>
-                <Text style={styles.incidentValue}>R$ 120,00</Text>
+                <Text style={styles.incidentValue}>{formatValue(incident.value)}</Text>
             </View>
 
             <View style={styles.contactBox}>
--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import { 
@@ -9,17 +9,96 @@ import {
     FlatList 
 } from 'react-native';
 
+import api from '../../services/api';
+
 import styles from './styles';
 
 import logoImg from '../../assets/logo.png';
 
 export default function Incidents() {
-
+    const [incidents, setIncidents] = useState([]);
+    const [total, setTotal] = useState(0);
     const navigation = useNavigation();
 
-    function navigateToDetails() {
-        navigation.navigate('Detail');
+    function navigateToDetails(incident) {
+        navigation.navigate('Detail', { incident });
     }
+
+    async function loadIncidents() {
+        // const response = await api.get('incidents');
+
+        // setIncidents(response.data);
+        // setTotal(response.headers['x-total-count']);
+
+        setIncidents([
+            {
+              "id": 2,
+              "title": "Caso 1",
+              "description": "Detalhes do Caso",
+              "value": 120,
+              "ong_id": "71b62f91",
+              "name": "APAD",
+              "email": "contato@teste.com",
+              "whatsapp": "001234123",
+              "city": "Rio do Sul",
+              "uf": "SC"
+            },
+            {
+              "id": 3,
+              "title": "Caso 1",
+              "description": "Detalhes do Caso",
+              "value": 120,
+              "ong_id": "303671f7",
+              "name": "APAD2",
+              "email": "contato@teste.com",
+              "whatsapp": "001234123",
+              "city": "Rio do Sul",
+              "uf": "SC"
+            },
+            {
+              "id": 4,
+              "title": "Caso 2",
+              "description": "Detalhes do Caso",
+              "value": 120,
+              "ong_id": "303671f7",
+              "name": "APAD2",
+              "email": "contato@teste.com",
+              "whatsapp": "001234123",
+              "city": "Rio do Sul",
+              "uf": "SC"
+            },
+            {
+              "id": 5,
+              "title": "Caso 3",
+              "description": "Detalhes do Caso",
+              "value": 120,
+              "ong_id": "303671f7",
+              "name": "APAD2",
+              "email": "contato@teste.com",
+              "whatsapp": "001234123",
+              "city": "Rio do Sul",
+              "uf": "SC"
+            },
+            {
+              "id": 6,
+              "title": "Caso 4",
+              "description": "Detalhes do Caso",
+              "value": 120,
+              "ong_id": "303671f7",
+              "name": "APAD2",
+              "email": "contato@teste.com",
+              "whatsapp": "001234123",
+              "city": "Rio do Sul",
+              "uf": "SC"
+            }
+          ])
+
+          setTotal(5)
+    }
+
+    useEffect(() => {
+        loadIncidents();
+    }, []);
 
     return (
         <View style={styles.container}>
@@ -27,7 +106,7 @@ export default function Incidents() {
             <View style={styles.header}>
                 <Image source={logoImg}/>
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>0</Text> casos.
+                    Total de <Text style={styles.headerTextBold}>{total} casos</Text>.
                 </Text>
             </View>
 
@@ -36,23 +115,23 @@ export default function Incidents() {
 
             <FlatList
                 style={styles.incidentList}
-                data={[1, 2, 3]}
-                keyExtractor={incident => String(incident)}
+                data={incidents}
+                keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
-                renderItem={() => (
+                renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
-                        <Text style={styles.incidentValue}>APAD</Text>
+                        <Text style={styles.incidentValue}>{incident.name}</Text>
 
                         <Text style={styles.incidentProperty}>CASO:</Text>
-                        <Text style={styles.incidentValue}>Caso</Text>
+                        <Text style={styles.incidentValue}>{incident.title}</Text>
 
                         <Text style={styles.incidentProperty}>VALOR:</Text>
-                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+                        <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</Text>
 
                         <TouchableOpacity 
                             style={styles.detailsButton}
-                            onPress={navigateToDetails}
+                            onPress={() => navigateToDetails(incident)}
                         >
                             <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                             <Feather name="arrow-right" size={16} color='#e02041'/>
